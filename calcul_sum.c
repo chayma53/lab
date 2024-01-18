8408e3768d47988131fc11583619c076c4f3d4b4
chayma53 <101133459+chayma53@users.noreply.github.com>
2024-01-18 15:30:26 +0100

    Create calcul_sum.c

---
+++ calcul_sum.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+void func(int sum) {
+  int a;
+  int b;
+  int sum = a+b;
+  printf("sum is %d", sum);
+}
+
+
+
+int main()
+{
+    printf("Enter a:\n");
+    scanf("%d", &a);
+    printf("enter b\n");
+    scanf("%d", &b);
+
+    return func(sum);
+
+}
